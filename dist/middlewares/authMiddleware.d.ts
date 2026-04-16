fdeeee95912c6a67f1b4c7f06021d0f3ed2506d2
ashisdutta <ashisdutta795@gmail.com>
2026-04-16 13:14:48 +0530

    chore: stop tracking dist folder

--- dist/middlewares/authMiddleware.d.ts
+++
@@ -1,4 +0,0 @@
-import { type Request, type Response, type NextFunction } from "express";
-export declare const protect: (req: Request, res: Response, next: NextFunction) => void;
-export declare const restrictTo: (...allowedRoles: string[]) => (req: Request, res: Response, next: NextFunction) => void;
-//# sourceMappingURL=authMiddleware.d.ts.map
--- dist/types/definitions.js
+++
@@ -1,3 +0,0 @@
-// Import the Role enum directly from your generated Prisma client
-import { Role } from "../generated/prisma/index.js";
-//# sourceMappingURL=definitions.js.map
